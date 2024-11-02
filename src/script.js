09814f7ea41a29481817661c467f85971cb740fa
mrquokka23 <armin.mere@gmail.com>
2024-11-02 16:20:30 +0200

    Added online json fetching code and did some styling changes.

--- src/script.js
+++ src/script.js
@@ -1,17 +1,84 @@
-document.addEventListener('DOMContentLoaded', function() {
+const API_ENDPOINT = 'https://api.jsonbin.io/v3/b/672603d9acd3cb34a8a13c3d'
+
+document.addEventListener('DOMContentLoaded', function () {
     const accountLink = document.getElementById('account-link');
     const dropdown = document.getElementById('dropdown');
 
     // Toggle the dropdown menu on click
-    accountLink.addEventListener('click', function(event) {
+    accountLink.addEventListener('click', function (event) {
         event.preventDefault(); // Prevent anchor default behavior
         dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
     });
 
     // Close the dropdown if the user clicks outside of it
-    window.addEventListener('click', function(event) {
+    window.addEventListener('click', function (event) {
         if (!accountLink.contains(event.target) && !dropdown.contains(event.target)) {
             dropdown.style.display = 'none';
         }
     });
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Access-Key' : '$2a$10$fDUywYuTxjBWIvJFfIp3Ju1ProrL2WD2eBwqnxitpklK0kuq9uWZy'
+        }
+    };
+
+    fetch(API_ENDPOINT, options)
+    .then((response) => response.json())
+    .then(json => {
+        if(window.location.pathname === '/index.html') {
+            const postscontainer = document.getElementsByClassName('posts');
+            console.log(postscontainer);
+            for(const postdata of json.record.posts){
+                let post = document.createElement("div");
+                post.className = 'post';
+
+                let headerdiv = document.createElement("div");
+                post.appendChild(headerdiv);
+                headerdiv.className = 'postHeader';
+
+                let userimage  = document.createElement("img");
+                headerdiv.appendChild(userimage);
+                userimage.className = 'userImage';
+                userimage.src = 'res/images/me.png';
+                
+                let date = document.createElement("h2");
+                headerdiv.appendChild(date)
+                date.className = 'date';
+                date.textContent = formatDate(new Date(postdata.created_at));
+
+                if(postdata.imageUrl != null) {
+                    let postimage = document.createElement("img");
+                    post.appendChild(postimage);
+                    postimage.className = 'postImage';
+                    postimage.src = postdata.imageUrl
+                }
+                
+                let postcontent  = document.createElement("p");
+                post.appendChild(postcontent);
+                postcontent.className = 'postText';
+                postcontent.textContent = postdata.content
+
+                let like = document.createElement("img");
+                post.appendChild(like);
+                like.className = 'like';
+                like.src = 'res/images/like.png';
+                
+                
+                postscontainer[0].appendChild(post);
+
+                console.log(postdata.imageUrl)
+            }
+        }
+    })
 });
+
+function formatDate(unformatted) {
+    return Intl.DateTimeFormat('en-GB', {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric'
+    }).format(unformatted);
+}
