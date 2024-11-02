5390cc0b85075f0d96845c3df411524b17925b00
mrquokka23 <armin.mere@gmail.com>
2024-11-02 20:34:55 +0200

    testing if local fetch works

--- src/script.js
+++ src/script.js
@@ -17,79 +17,77 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
     loadJSON();
-    
 
-     const options = {
-         method: 'GET',
-         headers: {
-             'Content-Type': 'application/json',
-             'X-Access-Key' : '$2a$10$fDUywYuTxjBWIvJFfIp3Ju1ProrL2WD2eBwqnxitpklK0kuq9uWZy'
-         }
-     };
-
-     fetch(API_ENDPOINT, options)
-     .then((response) => response.json())
-     .then(json => {
-         if(window.location.pathname === '/WAD-H1/' || window.location.pathname === '/WAD-H1/index.html') {
-             const postscontainer = document.getElementsByClassName('posts');
-             console.log(postscontainer);
-             for(const postdata of json.record.posts){
-                 let post = document.createElement("div");
-                 post.className = 'post';
-
-                 let headerdiv = document.createElement("div");
-                 post.appendChild(headerdiv);
-                 headerdiv.className = 'postHeader';
-
-                 let userimage  = document.createElement("img");
-                 headerdiv.appendChild(userimage);
-                 userimage.className = 'userImage';
-                 userimage.src = 'res/images/me.png';
-              
-                 let date = document.createElement("h2");
-                 headerdiv.appendChild(date)
-                 date.className = 'date';
-                 date.textContent = formatDate(new Date(postdata.created_at));
-
-                 if(postdata.imageUrl != null) {
-                     let postimage = document.createElement("img");
-                     post.appendChild(postimage);
-                     postimage.className = 'postImage';
-                     postimage.src = postdata.imageUrl
-                 }
-              
-                 let postcontent  = document.createElement("p");
-                 post.appendChild(postcontent);
-                 postcontent.className = 'postText';
-                 postcontent.textContent = postdata.content
-                 let like = document.createElement("img");
-                 post.appendChild(like);
-                 like.className = 'like';
-                 like.src = 'res/images/like.png';
-              
-                
-                 postscontainer[0].appendChild(post);
-
-                 console.log(postdata.imageUrl)
+    /*
+         const options = {
+             method: 'GET',
+             headers: {
+                 'Content-Type': 'application/json',
+                 'X-Access-Key' : '$2a$10$fDUywYuTxjBWIvJFfIp3Ju1ProrL2WD2eBwqnxitpklK0kuq9uWZy'
              }
-         }
-     })
+         };
+    
+         fetch(API_ENDPOINT, options)
+         .then((response) => response.json())
+         .then(json => {
+             if(window.location.pathname === '/WAD-H1/' || window.location.pathname === '/WAD-H1/index.html') {
+                 const postscontainer = document.getElementsByClassName('posts');
+                 for(const postdata of json.record.posts){
+                     let post = document.createElement("div");
+                     post.className = 'post';
+    
+                     let headerdiv = document.createElement("div");
+                     post.appendChild(headerdiv);
+                     headerdiv.className = 'postHeader';
+    
+                     let userimage  = document.createElement("img");
+                     headerdiv.appendChild(userimage);
+                     userimage.className = 'userImage';
+                     userimage.src = 'res/images/me.png';
+                  
+                     let date = document.createElement("h2");
+                     headerdiv.appendChild(date)
+                     date.className = 'date';
+                     date.textContent = formatDate(new Date(postdata.created_at));
+    
+                     if(postdata.imageUrl != null) {
+                         let postimage = document.createElement("img");
+                         post.appendChild(postimage);
+                         postimage.className = 'postImage';
+                         postimage.src = postdata.imageUrl
+                     }
+                  
+                     let postcontent  = document.createElement("p");
+                     post.appendChild(postcontent);
+                     postcontent.className = 'postText';
+                     postcontent.textContent = postdata.content
+                     let like = document.createElement("img");
+                     post.appendChild(like);
+                     like.className = 'like';
+                     like.src = 'res/images/like.png';
+                  
+                    
+                     postscontainer[0].appendChild(post);
+                 }
+             }
+         })
+    */
 });
 
-/*
+
 const LOCAL_JSON_PATH = "./res/json/myjson.json";
 
 //Potentially fixes the problem where github pages does not load
-async function loadJSON(){
-    try{
+async function loadJSON() {
+    try {
         const response = await fetch(LOCAL_JSON_PATH);
         const json = await response.json();
         console.log(window.lo);
 
-        if(window.location.pathname === '/WAD-H1/' || window.location.pathname === '/WAD-H1/index.html') {
+        if (window.location.pathname === '/WAD-H1/' || window.location.pathname === '/WAD-H1/index.html') {
             const postscontainer = document.getElementsByClassName('posts');
             console.log(postscontainer);
-            for(const postdata of json.posts){
+            for (const postdata of json.posts) {
                 let post = document.createElement("div");
                 post.className = 'post';
 
@@ -97,24 +95,24 @@ async function loadJSON(){
                 post.appendChild(headerdiv);
                 headerdiv.className = 'postHeader';
 
-                let userimage  = document.createElement("img");
+                let userimage = document.createElement("img");
                 headerdiv.appendChild(userimage);
                 userimage.className = 'userImage';
                 userimage.src = './res/images/me.png';
-                
+
                 let date = document.createElement("h2");
                 headerdiv.appendChild(date)
                 date.className = 'date';
                 date.textContent = formatDate(new Date(postdata.created_at));
 
-                if(postdata.imageUrl != null) {
+                if (postdata.imageUrl != null) {
                     let postimage = document.createElement("img");
                     post.appendChild(postimage);
                     postimage.className = 'postImage';
                     postimage.src = postdata.imageUrl
                 }
-                
-                let postcontent  = document.createElement("p");
+
+                let postcontent = document.createElement("p");
                 post.appendChild(postcontent);
                 postcontent.className = 'postText';
                 postcontent.textContent = postdata.content
@@ -123,20 +121,19 @@ async function loadJSON(){
                 post.appendChild(like);
                 like.className = 'like';
                 like.src = './res/images/like.png';
-                
-                
+
+
                 postscontainer[0].appendChild(post);
 
                 console.log(postdata.imageUrl)
             }
-    }
-
-        }catch(error){
-            console.error("Error fetching local JSON:", error)
         }
-    }
 
-*/
+    } catch (error) {
+        console.error("Error fetching local JSON:", error)
+    }
+}
+
 
 function formatDate(unformatted) {
     return Intl.DateTimeFormat('en-GB', {
