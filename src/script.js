140f400b98b8d5273e7d5c68607b80f7633af38f
mrquokka23 <armin.mere@gmail.com>
2024-11-02 20:31:26 +0200

    testing online fetching

--- src/script.js
+++ src/script.js
@@ -17,67 +17,66 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
     loadJSON();
-});
     
 
-    // const options = {
-    //     method: 'GET',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'X-Access-Key' : '$2a$10$fDUywYuTxjBWIvJFfIp3Ju1ProrL2WD2eBwqnxitpklK0kuq9uWZy'
-    //     }
-    // };
+     const options = {
+         method: 'GET',
+         headers: {
+             'Content-Type': 'application/json',
+             'X-Access-Key' : '$2a$10$fDUywYuTxjBWIvJFfIp3Ju1ProrL2WD2eBwqnxitpklK0kuq9uWZy'
+         }
+     };
 
-//     fetch(API_ENDPOINT, options)
-//     .then((response) => response.json())
-//     .then(json => {
-//         if(window.location.pathname === '/index.html') {
-//             const postscontainer = document.getElementsByClassName('posts');
-//             console.log(postscontainer);
-//             for(const postdata of json.record.posts){
-//                 let post = document.createElement("div");
-//                 post.className = 'post';
+     fetch(API_ENDPOINT, options)
+     .then((response) => response.json())
+     .then(json => {
+         if(window.location.pathname === '/WAD-H1/' || window.location.pathname === '/WAD-H1/index.html') {
+             const postscontainer = document.getElementsByClassName('posts');
+             console.log(postscontainer);
+             for(const postdata of json.record.posts){
+                 let post = document.createElement("div");
+                 post.className = 'post';
 
-//                 let headerdiv = document.createElement("div");
-//                 post.appendChild(headerdiv);
-//                 headerdiv.className = 'postHeader';
+                 let headerdiv = document.createElement("div");
+                 post.appendChild(headerdiv);
+                 headerdiv.className = 'postHeader';
 
-//                 let userimage  = document.createElement("img");
-//                 headerdiv.appendChild(userimage);
-//                 userimage.className = 'userImage';
-//                 userimage.src = 'res/images/me.png';
+                 let userimage  = document.createElement("img");
+                 headerdiv.appendChild(userimage);
+                 userimage.className = 'userImage';
+                 userimage.src = 'res/images/me.png';
+              
+                 let date = document.createElement("h2");
+                 headerdiv.appendChild(date)
+                 date.className = 'date';
+                 date.textContent = formatDate(new Date(postdata.created_at));
+
+                 if(postdata.imageUrl != null) {
+                     let postimage = document.createElement("img");
+                     post.appendChild(postimage);
+                     postimage.className = 'postImage';
+                     postimage.src = postdata.imageUrl
+                 }
+              
+                 let postcontent  = document.createElement("p");
+                 post.appendChild(postcontent);
+                 postcontent.className = 'postText';
+                 postcontent.textContent = postdata.content
+                 let like = document.createElement("img");
+                 post.appendChild(like);
+                 like.className = 'like';
+                 like.src = 'res/images/like.png';
+              
                 
-//                 let date = document.createElement("h2");
-//                 headerdiv.appendChild(date)
-//                 date.className = 'date';
-//                 date.textContent = formatDate(new Date(postdata.created_at));
+                 postscontainer[0].appendChild(post);
 
-//                 if(postdata.imageUrl != null) {
-//                     let postimage = document.createElement("img");
-//                     post.appendChild(postimage);
-//                     postimage.className = 'postImage';
-//                     postimage.src = postdata.imageUrl
-//                 }
-                
-//                 let postcontent  = document.createElement("p");
-//                 post.appendChild(postcontent);
-//                 postcontent.className = 'postText';
-//                 postcontent.textContent = postdata.content
+                 console.log(postdata.imageUrl)
+             }
+         }
+     })
+});
 
-//                 let like = document.createElement("img");
-//                 post.appendChild(like);
-//                 like.className = 'like';
-//                 like.src = 'res/images/like.png';
-                
-                
-//                 postscontainer[0].appendChild(post);
-
-//                 console.log(postdata.imageUrl)
-//             }
-//         }
-//     })
-// });
-
+/*
 const LOCAL_JSON_PATH = "./res/json/myjson.json";
 
 //Potentially fixes the problem where github pages does not load
@@ -137,7 +136,7 @@ async function loadJSON(){
         }
     }
 
-
+*/
 
 function formatDate(unformatted) {
     return Intl.DateTimeFormat('en-GB', {
